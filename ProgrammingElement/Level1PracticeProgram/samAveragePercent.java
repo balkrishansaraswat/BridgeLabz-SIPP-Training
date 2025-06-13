49dec3f3e97f2421d180dfeb94ef130faa9ae688
balkrishansaraswat <balkrishansaraswat7@gmail.com>
2025-06-13 17:52:02 +0530

    [Balkrishan] Add: level 1 programs

---
+++ ProgrammingElement/Level1PracticeProgram/samAveragePercent.java
@@ -0,0 +1,12 @@
+package BestProgrammingPractice.Level1PracticePrograms;
+
+public class samAveragePercent {
+    public static void main(String[] args) {
+        int mathMarks = 94;
+        int physicsMarks = 95;
+        int chemistryMarks = 96;
+        double averageMarks = (mathMarks + physicsMarks + chemistryMarks) / 3.0;
+        System.out.println("Sam’s average mark in PCM is " + averageMarks);
+    }
+
+}
